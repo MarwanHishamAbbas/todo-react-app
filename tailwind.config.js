883b549a783b344d2a0a01cfa9c47608e5f8bbe4
Marwan Hisham <marwanhiisham@outlook.com>
2022-02-16 00:03:31 +0200

    Set up tailwindcss with react

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#7367F0",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
